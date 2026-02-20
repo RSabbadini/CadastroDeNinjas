0aae821bc9c428dc95b6390b451b660f6218b259
Rafael Sabbadini <rafaelsabbadini8@gmail.com>
2026-02-20 17:02:25 -0300

    feat: Coloca o Lombock na classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Service/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Service/NinjaModel.java
@@ -1,6 +1,9 @@
 package dev.java10x.CadastroDeNinjas.Ninjas.Service;
 import dev.java10x.CadastroDeNinjas.Missoes.Service.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -8,6 +11,9 @@ import java.util.List;
 // ⬇️ Transforma a classe em uma entidade do db
 @Entity
 @Table(name= "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -21,35 +27,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missões_id") //chave estrangeira
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
