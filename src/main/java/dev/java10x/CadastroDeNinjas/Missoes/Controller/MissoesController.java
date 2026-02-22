fac6c565a14b9fd50a1ea2c328df7af5fdce28fe
Rafael Sabbadini <rafaelsabbadini8@gmail.com>
2026-02-22 17:21:20 -0300

    feat: Cria rotas do Missoes Controller com mock

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Controller/MissoesController.java
@@ -0,0 +1,29 @@
+package dev.java10x.CadastroDeNinjas.Missoes.Controller;
+import dev.java10x.CadastroDeNinjas.Ninjas.Service.NinjaModel;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "missoes listadas:";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missão alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missão deletada com sucesso";
+    }
+}
